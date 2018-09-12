fc1bb533da1ea05ecd2a1cd32bb931700985d231
AA047267 <37131020+AA047267@users.noreply.github.com>
2018-09-12 11:57:25 +0530

    Create index.php

---
+++ quiz/index.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div.container {
+            width: 100%;
+            border: 1px solid gray;
+}
+
+header, footer {
+            padding: 1em;
+            color: white;
+            background-color: red;
+            clear: left;
+            text-align: center;
+}
+
+nav {
+            float: left;
+            max-width: 160px;
+            margin: 0;
+            padding: 1em;
+}
+
+nav ul {
+            list-style-type: none;
+            padding: 0;
+}
+nav ul a {
+         text-decoration: none;
+}
+
+article {
+            margin-left: 170px;
+            border-left: 1px solid gray;
+            padding: 1em;
+            overflow: hidden;
+}
+
+</style>
+</head>
+<body>
+
+<div class="container">
+
+<header>
+   <h1>ConnectWorks-Ops</h1>
+</header>
+
+
+<nav>
+  <u1>
+    <li><a href="#">Bangalore</a></li>
+    <li><a href="#">Karnataka</a></li>
+  </ul>
+</nav>
+
+<article>
+  <h3 style="color:Tomato;">HELLO TEAM! THIS IS VERSION 2</h3>
+
+</article>
+
+
+
+</div>
+</body>
+</html>
