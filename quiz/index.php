9eacd9e604cb2e921fc70f58e350f448aedaa07a
AA047267 <37131020+AA047267@users.noreply.github.com>
2018-09-13 20:40:29 +0530

    Second update has been made.

--- quiz/index.php
+++ quiz/index.php
@@ -57,7 +57,7 @@ article {
 </nav>
 
 <article>
-  <h3 style="color:Tomato;">HELLO TEAM! THIS IS TO TEST JENKINS PIPELINE</h3>
+  <h3 style="color:Tomato;">HELLO TEAM! THIS IS TO TEST JENKINS PIPELINE AGAIN</h3>
 
 </article>
 
